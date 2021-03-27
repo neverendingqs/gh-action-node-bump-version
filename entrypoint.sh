ff02c61edf8e2d9d8165a305503735bfed11c82d
Marcelo Mourão <marcelo.rmourao@gmail.com>
2021-03-27 12:24:47 -0300

    Fix hub command to accept base branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,10 @@ GIT_USER_NAME=${1}
 GIT_USER_EMAIL=${2}
 PACKAGE_MANAGER=${3}
 VERSION_TYPE=${4}
-BRANCH=${5}
-PR_DESCRIPTION=${6}
-PUSH_OPTIONS=${7}
+BASE_BRANCH=${5}
+BRANCH=${6}
+PR_DESCRIPTION=${7}
+PUSH_OPTIONS=${8}
 
 if [ "${PACKAGE_MANAGER}" == 'npm' ]; then
   npm version --no-git-tag-version ${VERSION_TYPE}
@@ -27,6 +28,10 @@ else
   PR_BRANCH=chore/version-$(date +%s)
 fi
 
+if [ -z "${BASE_BRANCH}" ]; then
+  BASE_BRANCH="master"
+fi
+
 git config user.name ${GIT_USER_NAME}
 git config user.email ${GIT_USER_EMAIL}
 git checkout -b ${PR_BRANCH}
@@ -34,4 +39,4 @@ git commit -am "${DESCRIPTION}"
 git push ${PUSH_OPTIONS} origin ${PR_BRANCH}
 
 curl -fsSL https://github.com/github/hub/raw/master/script/get | bash -s 2.14.1
-bin/hub pull-request -m "${DESCRIPTION}"
+bin/hub pull-request -m "${DESCRIPTION}" -b "${BASE_BRANCH}"
